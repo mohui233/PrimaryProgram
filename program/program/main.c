3a660ea37ff9e289e293e67b333c0cfd36952a81
wangzhijie <1327342025@qq.com>
2022-06-07 16:00:16 +0800

    初次提交

---
+++ program/program/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  program
+//
+//  Created by 王志杰 on 2022/6/7.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
